6ca21b47fabcd84a9692f616596a03dec3294d09
mgaughan <mgaughan@proton.me>
2024-08-13 20:23:52 -0700

    tested analysis for extensions

--- main.py
+++ main.py
@@ -7,4 +7,7 @@ def main(project_name, project_vcs, commit_hash):
     download_repo.delete_repo(clone_location)
 
 if __name__ == "__main__":
-    main("ConfirmEdit", "https://gerrit.wikimedia.org/r/mediawiki/extensions/ConfirmEdit", "92bcb7f2a288")
+    main("Echo", "https://gerrit-replica.wikimedia.org/r/mediawiki/extensions/Echo", "d9aef933bb2f580fb646f33c131b559b3cec1552")
+    main("AbuseFilter", "https://gerrit-replica.wikimedia.org/r/mediawiki/extensions/AbuseFilter", "ad732457f02053cb30feef369e88dca215a5b073")
+    main("OAuth", "https://gerrit-replica.wikimedia.org/r/mediawiki/extensions/OAuth", "b820d2ad1361c240b7b5c3e8e5e9691f4b98f50a")
+    main("CheckUser", "https://gerrit-replica.wikimedia.org/r/mediawiki/extensions/CheckUser", "a3443da5504e660687b9363ff47c09b27d51b7ca")
---
+++ roster_demo.py
@@ -0,0 +1,4 @@
+#TODO:
+# [ ] split the overall number of contributors by the emails of their commits 
+# [ ] add to the json file presented by the AST analysis
+# [ ] (in different file) summarize the report information into CSV form for entire collection?
